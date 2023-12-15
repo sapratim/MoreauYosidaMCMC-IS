5df1674f438a505edd1d7f013af86a3f94fda744
Apratim Shukla <92173599+ApratimSh@users.noreply.github.com>
2023-12-15 14:30:04 +0530

    Update

--- IS_trendfiltering/trendfiltering_visualisation.R
+++ IS_trendfiltering/trendfiltering_visualisation.R
@@ -50,3 +50,46 @@ plot(y, type = "o", col = "red", main = "data vs posterior mean")
 lines(post_mean, col = "green")
 legend("topright", c("observed data", "posterior IS mean"), lty = 1,
        col = c("red", "green"), cex = 0.8, bty = "n")
+
+
+#  Quantile visualisation
+
+augm_mat <- cbind(mala_chain,weights)
+
+quant <- function(j)
+{
+  mat_ordered <- augm_mat[order(augm_mat[,j], decreasing = FALSE), ]
+  order_comp <- mat_ordered[,j]
+  weights_order <- mat_ordered[, length(y)+1]
+  bound_mat <- cbind(order_comp, weights_order)
+  return(bound_mat)
+}
+
+upper_quant <- numeric(length = length(y))
+lower_quant <- numeric(length = length(y))
+signif_level <- 0.025
+
+for (i in 1:length(y)) 
+ {
+  initial_mat <- quant(i)
+  mat_sum <- apply(initial_mat, 2, sum)
+  wts_prop <- initial_mat[,2]/mat_sum[2]
+  final_mat <- cbind(initial_mat[1,], cumsum(wts_prop))
+  lower_index <- min(which(final_mat[,2] >= signif_level))
+  upper_index <- min(which(final_mat[,2] >= (1 - signif_level)))
+  upper_quant[i] <- initial_mat[upper_index,1]
+  lower_quant[i] <- initial_mat[lower_index,1]
+}
+
+dataset <- data.frame(x, y, lower_quant, upper_quant)
+library("ggplot2")
+plot <- ggplot(dataset, aes(x, y)) + geom_point()
+conf_bands <- plot + geom_ribbon(aes(ymin = lower_quant, ymax = upper_quant),  
+              alpha = 0.2, fill="orange", color="orange")
+conf_bands
+
+# plot(y, type = "l", col = "red", main = "95 % credible interval")
+# lines(upper_quant, type = "l", col = "green")
+# lines(lower_quant, type = "l", col = "orange")
+# legend("topright", c("observed data", "upper quantile", "lower quantile"), lty = 1,
+#        col = c("red", "green", "orange"), cex = 0.8, bty = "n")
