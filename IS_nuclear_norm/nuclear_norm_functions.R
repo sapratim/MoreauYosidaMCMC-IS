67f8cf32a66026f305869feb78f8667db29e3857
Apratim Shukla <92173599+ApratimSh@users.noreply.github.com>
2024-02-11 16:12:16 +0530

    Update

--- IS_nuclear_norm/nuclear_norm_functions.R
+++ IS_nuclear_norm/nuclear_norm_functions.R
@@ -9,10 +9,11 @@ library(expm)
 library(foreach)
 library(doParallel)
 library(ks)
+library(SimTools)
 
 set.seed(11111)
-n <- 8
-a <- 2
+n <- 64
+a <- 8
 mat <- matrix(0, nrow = n, ncol = n)
 vec.mat <- rep(c(1, 0), each = a, times = n/(2*a))
 checker <- matrix(0, nrow = n, ncol = n)
@@ -63,7 +64,7 @@ log_target <- function(eta,x,lambda,y,sigma2,alpha)   # log target function of p
 #########  Soft threshold function
 
 softthreshold <- function(u, lambda) {       ####  u is a vector
-  return(sign(u)*sapply(u, FUN=function(x) {max(abs(u)-lambda,0)}))
+  return(sign(u)*sapply(u, FUN=function(x) {max(abs(x)-lambda,0)}))
 }
 
 ######### Proximity mapping
@@ -85,12 +86,6 @@ log_gradpi <- function(x,lambda,y,sigma2,alpha)  # gradient of log target
   return(-ans)
 }
 
-# dmvnorm_fn <- function(point, mu, mat, delta)
-# {
-#   diff <- point - mu
-#   exp_term <- (t(diff) %*% mat) %*% diff
-#   den_value <- -(exp_term/(2*delta))
-# }
 
 #### MYMALA sampling for covariance matrix estimation
 
@@ -129,7 +124,7 @@ mymala_cov_fn <- function(y, alpha, sigma2, iter, delta)  #pre-conditioned mala
       samp.mym[i,] <- samp_current
     }
     samp_current <- samp.mym[i,]
-    if(i %% 1000 == 0){
+    if(i %% (iter/10) == 0){
       j <- accept/iter
       print(cat(i, j))
     }
@@ -146,12 +141,12 @@ mymala <- function(y, alpha, sigma2, iter, delta)
   samp.mym <- matrix(0, nrow = iter, ncol = length(y))
   lambda <- lamb_coeff
   wts_is_est <- numeric(length = iter)
-  samp_current <- y
+  samp_current <- start_value
   samp.mym[1,] <- samp_current
-  g_val <- alpha*nucl_norm(samp_current) + sum((y - samp_current)^2)/(2*sigma2)
+  psi_val <- alpha*nucl_norm(samp_current) + sum((y - samp_current)^2)/(2*sigma2)
   prox_val.curr <- prox_func(samp_current, lambda, y, sigma2, alpha)
-  g_lambda_val <- - log_target(prox_val.curr,samp_current,lambda,y,sigma2,alpha)
-  wts_is_est[1] <- g_lambda_val - g_val
+  psi_lambda_val <- - log_target(prox_val.curr,samp_current,lambda,y,sigma2,alpha)
+  wts_is_est[1] <- psi_lambda_val - psi_val
  # U <- sqrtm(covmat)
 #  mat.inv <- solve(covmat)
   accept <- 0
@@ -175,20 +170,20 @@ mymala <- function(y, alpha, sigma2, iter, delta)
     {
       samp.mym[i,] <- samp_next
       prox_val.curr <- prox_val.next
-      g_val <- alpha*nucl_norm(samp_next) + sum((y - samp_next)^2)/(2*sigma2)
-      g_lambda_val <- - log_target(prox_val.next,samp_next,lambda,y,sigma2,alpha)
-      wts_is_est[i] <- g_lambda_val - g_val
+      psi_val <- alpha*nucl_norm(samp_next) + sum((y - samp_next)^2)/(2*sigma2)
+      psi_lambda_val <- - log_target(prox_val.next,samp_next,lambda,y,sigma2,alpha)
+      wts_is_est[i] <- psi_lambda_val - psi_val
       accept <- accept + 1
     }
     else
     {
       samp.mym[i,] <- samp_current
-      g_val <- alpha*nucl_norm(samp_current) + sum((y - samp_current)^2)/(2*sigma2)
-      g_lambda_val <- - log_target(prox_val.curr,samp_current,lambda,y,sigma2,alpha)
-      wts_is_est[i] <- g_lambda_val - g_val
+      psi_val <- alpha*nucl_norm(samp_current) + sum((y - samp_current)^2)/(2*sigma2)
+      psi_lambda_val <- - log_target(prox_val.curr,samp_current,lambda,y,sigma2,alpha)
+      wts_is_est[i] <- psi_lambda_val - psi_val
     }
     samp_current <- samp.mym[i,]
-    if(i %% 1000 == 0){
+    if(i %% (iter/10) == 0){
       j <- accept/iter
       print(cat(i, j))
     }
@@ -204,7 +199,7 @@ px.mala <- function(y, alpha, sigma2, iter, delta)
 {
   samp.pxm <- matrix(0, nrow = iter, ncol = length(y))
   lambda <- lamb_coeff
-  samp_current <- y
+  samp_current <- start_value
   samp.pxm[1,] <- samp_current
   accept <- 0
   # U <- sqrtm(covmat)
@@ -233,7 +228,7 @@ px.mala <- function(y, alpha, sigma2, iter, delta)
       samp.pxm[i,] <- samp_current
     }
     samp_current <- samp.pxm[i,]
-    if(i %% 1000 == 0){
+    if(i %% (iter/10) == 0){
       j <- accept/iter
       print(cat(i, j))
     }
@@ -250,12 +245,12 @@ myhmc <- function(y, alpha, sigma2, iter, eps_hmc, L)
   samp.hmc <- matrix(0, nrow = iter, ncol = length(y))
   lambda <- lamb_coeff
   wts_is_est <- numeric(length = iter)
-  samp <- y
-  samp.hmc[1,] <- y
-  g_val <- alpha*nucl_norm(samp) + sum((y - samp)^2)/(2*sigma2)
+  samp <- start_value
+  samp.hmc[1,] <- samp
+  psi_val <- alpha*nucl_norm(samp) + sum((y - samp)^2)/(2*sigma2)
   proxval_curr <- prox_func(samp, lambda, y, sigma2, alpha)
-  g_lambda_val <- - log_target(proxval_curr,samp,lambda,y,sigma2,alpha)
-  wts_is_est[1] <- g_lambda_val - g_val
+  psi_lambda_val <- - log_target(proxval_curr,samp,lambda,y,sigma2,alpha)
+  wts_is_est[1] <- psi_lambda_val - psi_val
   mom_mat <- matrix(rnorm(iter*length(y)), nrow = iter, ncol = length(y))
   accept <- 0
   
@@ -288,20 +283,20 @@ myhmc <- function(y, alpha, sigma2, iter, eps_hmc, L)
     {
       samp.hmc[i,] <- samp
       proxval_curr <- proxval_prop
-      g_val <- alpha*nucl_norm(samp) + sum((y - samp)^2)/(2*sigma2)
-      g_lambda_val <- - log_target(proxval_prop,samp,lambda,y,sigma2,alpha)
-      wts_is_est[i] <- g_lambda_val - g_val
+      psi_val <- alpha*nucl_norm(samp) + sum((y - samp)^2)/(2*sigma2)
+      psi_lambda_val <- - log_target(proxval_prop,samp,lambda,y,sigma2,alpha)
+      wts_is_est[i] <- psi_lambda_val - psi_val
       accept <- accept + 1
     }
     else
     {
       samp.hmc[i,] <- q_current
-      g_val <- alpha*nucl_norm(q_current) + sum((y - q_current)^2)/(2*sigma2)
-      g_lambda_val <- - log_target(proxval_curr,samp,lambda,y,sigma2,alpha)
-      wts_is_est[i] <- g_lambda_val - g_val
+      psi_val <- alpha*nucl_norm(q_current) + sum((y - q_current)^2)/(2*sigma2)
+      psi_lambda_val <- - log_target(proxval_curr,q_current,lambda,y,sigma2,alpha)
+      wts_is_est[i] <- psi_lambda_val - psi_val
       samp <- q_current
     }
-    if(i %% 1000 == 0){
+    if(i %% (iter/10) == 0){
       j <- accept/iter
       print(cat(i, j))}
   } 
@@ -316,7 +311,7 @@ pxhmc <- function(y, alpha, sigma2, iter, eps_hmc, L)
 {
   samp.hmc <- matrix(0, nrow = iter, ncol = length(y))
   lambda <- lamb_coeff
-  samp <- y
+  samp <- start_value
   samp.hmc[1,] <- samp
   mom_mat <- matrix(rnorm(iter*length(y)), nrow = iter, ncol = length(y))
   accept <- 0
@@ -353,7 +348,7 @@ pxhmc <- function(y, alpha, sigma2, iter, eps_hmc, L)
       samp.hmc[i,] <- q_current
       samp <- q_current
     }
-    if(i %% 1000 == 0){
+    if(i %% (iter/10) == 0){
       j <- accept/iter
       print(cat(i, j))}
   } 
@@ -361,46 +356,3 @@ pxhmc <- function(y, alpha, sigma2, iter, eps_hmc, L)
   object <- list(samp.hmc, acc_rate)
   return(object)
 }
-
-
-##########################################################################
-##################  Program run ##########################################
-##########################################################################
-
-
-iter <- 1e4
-lamb_coeff <- 0.00001
-sigma2_hat <- 0.01
-alpha_hat <- 1.15/sigma2_hat
-step <- 0.0000001
-step_pxm <- 0.0000005
-
-result_is <- mymala(y=y, alpha = alpha_hat, sigma2 = sigma2_hat, iter = iter, delta = step)
-result_pxm <- px.mala(y=y, alpha = alpha_hat, sigma2 = sigma2_hat, iter = iter, delta = step_pxm)
-
-result_ishmc <- myhmc(y=y, alpha = alpha_hat, sigma2 = sigma2_hat, 
-                               iter = iter, eps_hmc = 0.0001, L=10)
-result_pxhmc <- pxhmc(y=y, alpha = alpha_hat, sigma2 = sigma2_hat, 
-                      iter = iter, eps_hmc = 0.0001, L=10)
-
-chain <- result_is[[1]]
-weights <- result_is[[2]]
-hist(weights)
-
-
-############# Posterior mean
-
-weight_mat <- matrix(0, nrow = 1e4, ncol = length(y))
-for (i in 1:1e4) {
-  weight_mat[i,] <- chain[i,]*exp(weights[i])
-}
-num_sum <- apply(weight_mat, 2, sum)
-weights_sum <- sum(exp(weights))
-post_mean <- num_sum/weights_sum
-
-
-############ Image visualisation
-chain1 <- result_pxm
-image(matrix(post_mean, nrow = n, ncol = n), col = gray.colors(4, start = 0, end = 1), axes = FALSE)
-library(SimTools)
-traceplot(chain[, c(60:64)])
---
+++ IS_nuclear_norm/pre_conditioned_chain.R
@@ -0,0 +1,12 @@
+
+source("nuclear_norm_functions.R")
+iter_pc_chain <- 1e5
+lamb_coeff <- 0.00001
+sigma2_hat <- 0.01
+alpha_hat <- 1.15/sigma2_hat
+step_pcm <- 0.00008 
+
+markov_chain <- mymala_cov_fn(y=y, alpha = alpha_hat, sigma2 = sigma2_hat,
+                              iter = iter_pc_chain, delta = step_pcm)
+start_value <- markov_chain[iter_pc_chain,]
+save(start_value, file = "start_value.Rdata")
