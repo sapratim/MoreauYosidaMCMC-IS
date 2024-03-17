ff261c35481ef999a3807dbfe32ffb5c392b1250
Apratim Shukla <92173599+ApratimSh@users.noreply.github.com>
2024-03-17 06:25:12 -0500

    Partial update

--- Poisson_random_effects_model/random_effects.R
+++ Poisson_random_effects_model/random_effects.R
@@ -1,4 +1,6 @@
 ####  Poisson random effects model
+#### The function evaluated below of targets and the derivatives are omitting constant
+#### of proportionality.
 
 rm(list = ls())
 set.seed(12345)
@@ -9,15 +11,17 @@ sigma_eta <- 3
 one_mat <- rep(1, I)
 data <- matrix(0, nrow = I, ncol = ni_s)
 identity_mat <- diag(1, I, I)
+lambda <- 0.01
 mu <- rnorm(1, 0, c)
+eta_vec <- rnorm(I, mu, sigma_eta)
+tol_nr <- 0.01
 for (j in 1:ni_s)       # data generation
   {
-  eta_vec <- rnorm(I, mu, sigma_eta)
   data[,j] <- rpois(I, exp(eta_vec))
 }
 data
 
-true_target <- function(eta, mu, data)
+true_target <- function(eta, mu, data)   # log of true target i.e. log(p(eta, mu|y))
 {
   densval <- sum(eta^2)/(2*sigma_eta^2) - mu*sum(eta)/(sigma_eta^2) + 
                       ni_s*sum(exp(eta)) - sum(eta*apply(data, 1, sum)) +
@@ -25,33 +29,45 @@ true_target <- function(eta, mu, data)
   return(-densval)
 }
 
-proxfunc <- function(lambda, eta_initial, mu_initial)
+true_grad_vec <- function(mu, sigma, eta)  # function evaluates gradient of log target
+{
+  term_exp_eta <- (ni_s*one_mat)*exp(eta)
+  term_y <- apply(data, 1, sum)
+  term_mu_sigma <- (mu/(sigma^2))*one_mat
+  grad_vec <- - eta/(sigma^2) - term_exp_eta + 
+    term_y + term_mu_sigma
+  return(grad_vec)
+}
+
+true_hessian_eta <- function(sigma, eta)
+{
+  term_exp_diag <- 1/(sigma^2) + (ni_s*one_mat)*exp(eta)
+  eta_hessian <- - diag(term_exp_diag)
+}
+
+proxfunc <- function(eta, mu, lambda, eta_initial, mu_initial, sigma)
 {
   # For eta's
-  term_exp_eta <- (ni_s*t(one_mat))%*%exp(eta_initial)
-  term_y <- apply(data, 1, sum)
-  term_mu_sigma <- (mu/(sigma_eta^2))*one_mat
-  grad_vec <- - eta_initial/(sigma_eta^2) - term_exp_eta + 
-                term_y + term_mu_sigma
-  eta_hessian <- - (1/(sigma_eta^2))%*%identity_mat - diag(term_exp_eta)
-  while (sum(grad_vec^2) > 0.0001) 
+  grad_vec <- true_grad_vec(mu_initial, sigma, eta_initial) + (eta_initial - eta)/lambda
+  eta_hessian <- true_hessian_eta(sigma, eta_initial) + 1/lambda
+  while (sum(grad_vec^2) > tol_nr) 
     {
     eta_next <- eta_initial - solve(eta_hessian)%*%grad_vec
-    term_exp_eta <- (ni_s*t(one_mat))%*%exp(eta_next)
-    grad_vec <- - eta_next/(sigma_eta^2) - term_exp_eta + 
-                      term_y + term_mu_sigma
-    eta_hessian <- - (1/(sigma_eta^2))%*%identity_mat - diag(term_exp_eta)
+    grad_vec_next <- true_grad_vec(mu_initial, sigma, eta_next) + (eta_next - eta)/lambda
+    eta_hessian_next <- true_hessian_eta(sigma, eta_next) + 1/lambda
     eta_initial <- eta_next
+    grad_vec < grad_vec_next
   }
-  mu_grad_vec <- mu_initial*I*(1/(sigma_eta^2) + 1/(c^2)) - 
-                           sum(abs(eta_initial))/(sum(sigma_eta^2))
-  mu_hessian <- I*(1/(sigma_eta^2) + 1/(c^2))
-  while (sum(mu_grad_vec^2) > 0.0001) 
+  mu_grad_vec <- mu_initial*I*(1/(sigma^2) + 1/(c^2)) - 
+                           sum(abs(eta_initial))/(sigma^2) + (mu - mu_initial)/lambda
+  mu_hessian <- I*(1/(sigma^2) + 1/(c^2)) + 1/lambda
+  while (sum(mu_grad_vec^2) > tol_nr) 
     {
     mu_next <- mu_initial - mu_grad_vec/mu_hessian
-    mu_grad_vec <- mu_next*I*(1/(sigma_eta^2) + 1/(c^2)) - 
-      sum(abs(eta_initial))/(sum(sigma_eta^2))
+    mu_grad_vec_next <- mu_next*I*(1/(sigma^2) + 1/(c^2)) - 
+      sum(abs(eta_initial))/(sum(sigma^2)) + (mu - mu_next)/lambda
     mu_initial <- mu_next
+    mu_grad_vec <- mu_
   }
   optima <- rbind(eta_initial, mu_initial)
   return(optima)
@@ -63,3 +79,9 @@ MY_env <- function(eta, mu, data, optima, lambda)
   value <- true_target(eta, mu, data) + sum(term^2)/lambda
   return(-value)
 }
+
+eta_start <- eta_vec + rnorm(I, 0, 2)
+mu_start <- 8
+proxfunc(eta_vec, mu, lambda = lambda, eta_initial = eta_start, 
+         mu_initial = mu_start, sigma = sigma_eta)
+
