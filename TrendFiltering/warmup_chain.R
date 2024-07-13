245a2a7e91da84bd779eb64f702034a6def20006
Apratim Shukla <s.apratim@gmail.com>
2024-07-13 16:44:34 +0530

    update

--- TrendFiltering/warmup_chain.R
+++ TrendFiltering/warmup_chain.R
@@ -5,6 +5,6 @@ lamb_coeff <- 0.001
 D_mat <- getD(k=1, n=1e2, x)   #  D matrix
 delta_warmup <- .0008
 warmup_chain <- px.mala(y, alpha_hat, sigma2_hat, k=1, grid=x, iter = iter_warmup, 
-                        delta = delta_warmup, start = pcm_last_iter)
+                        delta = delta_warmup, start = y)
 warmup_end_iter <- warmup_chain[iter_warmup,]
 save(warmup_end_iter, file = "warmup.Rdata")
